cbf71fa89fe06193fee11af6e653c7d1d7064d15
SGruel <simeon.gruel@gmail.com>
2017-11-08 13:42:25 +0100

    Ajout d'une première methode de calcul des moindres carrés.

--- Moindres_Carres/vitesse_MC.py
+++ Moindres_Carres/vitesse_MC.py
@@ -10,5 +10,36 @@ def moindre_carres(data,covariance=False,*periode):
     :type data: numpy.array
     :type covariance:numpy.array
     :type periode list
-    :return: liste des paramêtres déterminés avec leur écart-types, un denier élément correspond à l'écartype au carré total des moindres carrés.
+    :return: liste des paramêtres déterminés pour chacun des axes avec leur écart-types, les 3 derniers éléments correspondent à l'écartype au carré total des moindres carrés.
+    """
+
+
+
+
+
+
+
+
+
+
+def matrice_B(data,):
+    """
+    creation de la matrice des observations à partir de la donnée formatée, sous forme de colonne (East) ou (North) ou(Up) celon l'axe sur lequel on mesure la vitesse
+                                                                                                  
+    :param data: jeu de données formaté
+    :return: matrice colonne
+    """
+
+
+
+def matrice_A(data,*periode):
+    """
+    creation de la matrice d'estimation des paramêtre;
+    autant de ligne que d'observation;
+    en colonne: ( 1  t-t0   (sinusoide des périodes) (indice de saut nul si différent  1 sinon)
+    
+    
+    :param data: jeu formaté
+    :param periode: liste des périodes  utilisée
+    :return: matrice
     """
