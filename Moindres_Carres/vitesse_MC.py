bd94007d8a6acfae5b3570cf37bc8b4894b7f130
SGruel <simeon.gruel@gmail.com>
2017-11-08 16:37:42 +0100

    Ajout des fonctions de creation de matrice pour cas  periodicité multiple + plusieurs saut dans un signal

--- Moindres_Carres/vitesse_MC.py
+++ Moindres_Carres/vitesse_MC.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def moindreCarres(data,covariance=False,*periode):
+
+def moindreCarres(data, covariance=False, *periode):
     """
     Renvoie le résultat d'un traitement par moindre carrés d'un jeu de données formaté par la méthode formatage. 
     Les parametres d'entrée sont envoyés par la fonction traitement.
@@ -14,56 +15,88 @@ def moindreCarres(data,covariance=False,*periode):
     """
 
 
-
-
-
-
-
-
-
-
-def matriceB(data,):
+def matriceB(data, axe):
     """
     creation de la matrice des observations à partir de la donnée formatée, sous forme de colonne (East) ou (North) ou(Up) celon l'axe sur lequel on mesure la vitesse
                                                                                                   
     :param data: jeu de données formaté
+    :param axe: axe choisi "East","North" ou "Up"
+    
     :return: matrice colonne
     """
+    indice = 0
+    if axe == "North":
+        indice = 3
+    elif axe == "East":
+        indice = 2
+    else:
+        indice = 4
+
+    B = np.zeros((len(data), 1))
+    B[:, 0] = data[:, indice]
+    return B
 
 
-
-def matriceA(data,*periode):
+def matriceA(data, t0, *periode):
     """
-    creation de la matrice d'estimation des paramêtre;
+    creation de la matrice d'estimation des paramètre;
     autant de ligne que d'observation;
-    en colonne: ( 1  t-t0   (sinusoide des périodes) (indice de saut nul si différent  1 sinon)
+    en colonne: ( 1  t-t0  (cosinus des période) alterné avec (sinusoide des périodes) (indice de saut nul si différent  1 sinon)
     
     
     :param data: jeu formaté
     :param periode: liste des périodes  utilisée
+    :param t0: temps de reference
     :return: matrice
     """
+    nb_serie = int(data[-1:0])
+    A = np.zeros((len(data), 2 + 2 * len(*periode)) + nb_serie)
+    A[:0] = 1
+    A[:1] = (data[:1] - t0) / 365.25
+    for i in range(len(*periode)):
+        #implementation des coefficient de périodicité
+        A[:i + 2] = np.cos(((data[:1] - t0) / 365.25) / periode[i])
+        A[:i + 3] = np.sin(((data[:1] - t0) / 365.25) / periode[i])
+    #indice de saut
+    A[:int(data[:0]) + 1 + 2 * len(*periode)] = 1
 
-def matriceP (data,covariance=False):
+    return A
+
+def matriceP(data, axe,covariance=False):
     """
     Creation de la matrice de poids, prend en compte l'ajout d'un matrice de covariance  sinon utilise les écart-types sur le mesure de la donnée formatée
+    
+    (ne prends pas encore en compte la covariance)8/11
     :param data: jeu de  donnée formaté 
     :param covariance:  matrice  fourni par l'utilisateur , doit etre adaptée au jeu de donnée.
+    :param axe: axe choisi "East","North" ou "Up"
     :return: matrice de poids
+    
+    
     """
-
-
-
-
-def matriceNormaleLigne(A,P):
+    P=np.zeros((len(data),len(data)))
+    indice = 0
+    if axe == "North":
+        indice = 6
+    elif axe == "East":
+        indice = 5
+    else:
+        indice = 7
+    if covariance == False:
+        di=np.diag_indices_from(P)
+        P[di] = 1/data[:indice]**2
+    return P
+        
+def matriceNormaleLigne(A, P):
     """
     Fonction de sous-calcul de la matrice normale pour le traitement ligne par ligne. effectue le produit AtPA sur une ligne de la matrice A.
     :param A: ligne de la matrice A 
     :param P: poids de la mesure correspondant à la ligne A
     :return: matrice carrée de la longueur de A
     """
-
-def vecteurKligne(A,P,B):
+    Nligne=np.dot(np.transpose(A),P*A)
+    return Nligne
+def vecteurKligne(A, P, B):
     """
     Fonction de calcul d'un élément du vecteur K . Le calcul est fai par ligne  de manière  à alleger ce dernier.
     :param A: ligne de la matrice A
@@ -71,5 +104,5 @@ def vecteurKligne(A,P,B):
     :param B:  observation
     :return: matrice composé d'un élément unique
     """
-
+    Kligne=np.dot(np.transpose(A),P*B)
 
