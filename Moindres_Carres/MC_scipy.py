1fe070c22585dcb561334407e86b2d224a0e6ffb
hulecom <hugo.lecomte1@gmail.com>
2017-11-22 09:36:52 +0100

    scipy

--- Moindres_Carres/MC_scipy.py
+++ Moindres_Carres/MC_scipy.py
@@ -12,20 +12,24 @@ def test_MC(data):
         T = 365.25
         total_saut = 0
         for i in range(6, 6 + len(saut)):
-            total_saut += x[6+i]*delta(t-saut[i])
+            total_saut += x[i]*delta(t-saut[i])
         return x[0] + x[1] * (t - t0) + x[2] * np.cos((t - t0) / T) + x[3] * np.sin((t - t0) / T) + x[4] * \
                 np.cos(2 * (t - t0) / T) + x[5] * np.sin(2 * (t - t0) / T) + total_saut - pos
-    x0 = np.ones(7)
+    compt = 0
+    for i in range(1,len(data)):
+        if data[i][0] != data[i-1][0]:
+            compt += 1
+    x0 = np.ones(6 + compt)
     t = []
     pos = []
     saut = []
     for i in range(len(data)):
         t.append(data[i][1])
-        R.append(data[i][2])
+        pos.append(data[i][2])
         if i>0 and data[i][0]>data[i-1][0]:
             saut.append(data[i][1])
     t = np.array(t)
-    R = np.array(R)
+    pos = np.array(pos)
     t0 = np.array(len(data) * [t[abs(len(t)/2)]])
     saut = np.array(len(data) * [saut])
 
