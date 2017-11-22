ff2f31538aafb7f255a5463415ac74cb2a6e5e2b
hulecom <hugo.lecomte1@gmail.com>
2017-11-22 13:28:41 +0100

    main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from Traitement.graphique import graphiqueMidas, graphiqueUnique
-from Moindres_Carres.MC_scipy import test_MC
+import Traitement.graphique as gr
+#from Moindres_Carres.MC_scipy import test_MC
 from Traitement.formatage import formatage
 
 path = "C:\\Users\\Hul\\Desktop\\Projet_recherche\\TOUL_igs.xyz"
 
 data = formatage(path)
-test_MC(data)
-#graphiqueMidas(path)
+#test_MC(data)
+#gr.graphiqueMidas(path)
+gr.graphiqueTot(path)
